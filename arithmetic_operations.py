8c05f32880e8d988fe31a59992e4bb43f50bd419
AnjanaCM <cmanjana1503@gmail.com>
2026-02-05 20:10:09 +0530

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,15 @@
+def main():
+    a=10
+    b=20
+    sum=a+b
+    sub=a-b
+    mul=a*b
+    div=a/b
+    mod=a%b
+    print(sum)
+    print(sub)
+    print(mul)
+    print(div)
+    print(mod)
+if __name__ == "__main__":
+    main()
