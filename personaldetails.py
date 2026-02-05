c1753b0673d6e85dea0cfe135c92ef2c5efaf47b
AnjanaCM <cmanjana1503@gmail.com>
2026-02-05 20:11:04 +0530

    Add files via upload

---
+++ personaldetails.py
@@ -0,0 +1,16 @@
+def main():
+    name="Anjana C M"
+    Branch="MCA"
+    Semister="4th semister"
+    College="UBDT Davanagere"
+    mob_no="000786987"
+    email="cmanjana1503@gmail.com"
+    print("Personal_details")
+    print(name)
+    print(Branch)
+    print(Semister)
+    print(College)
+    print(mob_no)
+    print(email)
+if __name__ == "__main__":
+    main()
